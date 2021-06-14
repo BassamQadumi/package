9168157355a4f41c13f6c586b119f8ccaf81f450
Bassam Qadumi <62744239+BassamQadumi@users.noreply.github.com>
2021-06-14 06:10:18 +0000

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,19 @@
+!#/usr/bin/bash
+# Adding the repositorys needed
+sudo add-apt-repository main -yy
+sudo add-apt-repository universe -yy
+sudo add-apt-repository restricted -yy
+sudo add-apt-repository multiverse -yy
+# letting the system update the repository file
+sudo apt update -yy
+# install (curl) and (wget)
+sudo apt install curl wget lolcat figlet -yy
+# update all installed package
+sudo apt upgrade -yy
+# downloading astronaut jellyfish wallpaper
+sudo wget https://i.pinimg.com/originals/2f/f2/6a/2ff26a3dd03271d1764c36af3ff448de.jpg
+# moving the wallpaper to Desktop
+sudo mv 2ff26a3dd03271d1764c36af3ff448de.jpg /$HOME/Desktop
+# BANNER
+sudo figlet created by Bassam Qadumi | lolcat
+
